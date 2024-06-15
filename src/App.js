bd8d909cec748f6b18dfdd156eb18ac364847f10
kishanpaneri412 <kishanpaneri412@gmail.com>
2024-06-15 13:21:32 +0530

    api initial commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/learnhooks" element={<LearnHooks />} />
         </Route>
+        
       </Routes>
     </BrowserRouter>
   );
